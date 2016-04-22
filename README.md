565583075b373f99422495fb0821b44e57181b90
Amit Navindgi <Amit Navindgi>
2016-04-22 22:41:28 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # npm-amitnavindgi
+
+I don't know why I made this
+
+But hey...here's how you can use it
+
+var amit = require('amitnavindgi')
+console.log(amitnavindgi.name.first) // 'Amit
+//...etc
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: {
+    first: 'Amit',
+    last: 'Navindgi',
+  },
+  birthday: '1992-07-17',
+  twitter: 'https://twitter.com/amitnavindgi',
+  github: 'https://github.com/amitnavindgi',
+  email: 'amitnavindgi@gmail.com',
+}
